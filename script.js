10e3210e4b70d75a39ac594c5ce5a09714b7614d
nicoleabrand <nicoleabrand13@gmail.com>
2023-12-07 08:39:08 -0300

    .

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function insert( valor ){
 
 function clean() {
     resultado.innerHTML = ' ';
+    confirmar.focus();
 }
 
 
@@ -23,4 +24,37 @@ function confirma() {
     if(resultado.textContent != 'Erro') {
         document.getElementById("resultado").innerHTML = eval(resultado.innerHTML)
     }
+}
+function addteclado() {
+    document.addEventListener("keypress", (e) => {
+        if (e.key == "1") {
+            insert("1");
+        }
+        else if (e.key == "2") {
+            insert("2");
+        }
+        else if (e.key == "3") {
+            insert("3");
+        }
+        else if (e.key == "4") {
+            insert("4");
+        }
+        else if (e.key == "5") {
+            insert("5");
+        }
+        else if (e.key == "6") {
+            insert("6");
+        }
+        else if (e.key == "7") {
+            insert("7");
+        }
+        else if (e.key == "8") {
+            insert("8");
+        }
+        else if (e.key == "9") {
+            insert("9");
+        }
+        else if (e.key == "0") {
+            insert("0");
+        }})  
 }
